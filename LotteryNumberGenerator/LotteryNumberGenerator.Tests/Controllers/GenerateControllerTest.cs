cc19234517959c638655ee565428d1c28b52586b
EddCodesDotNet <hey@eddcodes.net>
2019-01-27 14:05:28 +0000

    App commit

---
+++ LotteryNumberGenerator/LotteryNumberGenerator.Tests/Controllers/GenerateControllerTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LotteryNumberGenerator.Controllers;
+
+namespace LotteryNumberGenerator.Tests.Controllers
+{
+    [TestClass]
+    public class GenerateControllerTest
+    {
+        [TestMethod]
+        public void ReturnsCorrectNumberOfValues()
+        {
+            // Arrange
+            GenerateController controller = new GenerateController();
+
+            // Act
+            IEnumerable<int> result = controller.GetNumbers();
+
+            // Assert
+            Assert.AreEqual(result.Count(), 6);
+        }
+
+        [TestMethod]
+        public void AllReturnedValuesAreUnique()
+        {
+            // Arrange
+            GenerateController controller = new GenerateController();
+
+            // Repeat enough times to verify test
+            for (int i = 0; i < 1000; i++)
+            {
+                // Act
+                IEnumerable<int> result = controller.GetNumbers();
+
+                // Assert
+                Assert.IsTrue(result.Distinct().Count() == result.Count());
+            }
+        }
+    }
+}
---
+++ LotteryNumberGenerator/LotteryNumberGenerator/Controllers/GenerateController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace LotteryNumberGenerator.Controllers
+{
+    public class GenerateController : ApiController
+    {
+        private static readonly Random random = new Random();
+
+        // GET api/generate
+        public IEnumerable<int> GetNumbers()
+        {
+            var values = Generate();
+            return values;
+        }
+
+        private IEnumerable<int> Generate()
+        {
+            List<int> numbers = new List<int>();
+
+            while (numbers.Count < 6)
+            {
+                int randomValue = random.Next(1, 50);
+                if (!numbers.Contains(randomValue))
+                {
+                    numbers.Add(randomValue);
+                }
+            }
+
+            numbers.Sort();
+            return numbers;
+        }
+    }
+}
